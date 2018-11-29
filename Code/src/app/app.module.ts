89fe7d6331b7c7c9d49963d361ac3fc68e9a51ef
Thomas Miller <thomas.miller01.mail@gmail.com>
2018-11-29 19:43:01 +0100

    Basic Login Scripts included

--- Code/src/app/app.module.ts
+++ Code/src/app/app.module.ts
@@ -1,27 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { mainComponent } from './Components/public/main/mainComponent';
 import { privateMainComponent } from './Components/private/main/privateMainComponent';
+import { LoginComponent } from './Components/public/login/login.component';
+
+import { AuthGuard } from './guards/auth-guard.service';
+import { JwtHelper } from 'angular2-jwt';
 
 @NgModule({
   declarations: [
     AppComponent,
     mainComponent,
     privateMainComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule,    
+    BrowserModule,  
+    FormsModule,  
+    HttpClientModule,
+    HttpModule,
+    CommonModule,
     RouterModule.forRoot([
 			{ path: '', redirectTo: 'main', pathMatch: 'full' },
       { path: 'main', component: mainComponent },
-      { path: 'private', component: privateMainComponent },			
+      { path: 'private', component: privateMainComponent, canActivate: [AuthGuard], },			
     ]),    
   ],
   schemas: [NO_ERRORS_SCHEMA],
-  providers: [],
+  providers: [JwtHelper, AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
