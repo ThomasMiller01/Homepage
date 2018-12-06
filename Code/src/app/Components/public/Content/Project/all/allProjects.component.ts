198ba16aefcd680f0f97741767bf0e5b1fd188f5
Thomas Miller <thomas.miller01.mail@gmail.com>
2018-12-06 14:34:37 +0100

    error, on webserver images don't load correctly

--- Code/src/app/Components/public/Content/Project/all/allProjects.component.ts
+++ Code/src/app/Components/public/Content/Project/all/allProjects.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './allProjects.component.html'
+  templateUrl: './allProjects.component.html',
+  styleUrls: ['../../../../../../assets/scss/style.scss']
 })
 export class AllProjectsComponent { }
