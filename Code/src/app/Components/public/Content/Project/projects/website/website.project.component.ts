fe68d7d45f60bc5380040d27893d333aa6765b58
Thomas <thomas.miller01.mail@gmail.com>
2019-01-25 13:07:59 +0100

    added gallery to project_website page

--- Code/src/app/Components/public/Content/Project/projects/website/website.project.component.ts
+++ Code/src/app/Components/public/Content/Project/projects/website/website.project.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 
+declare var photoSwipeSetup: any;
+
 @Component({
   templateUrl: './website.project.component.html'
 })
-export class WebsiteProjectComponent { }
+
+export class WebsiteProjectComponent { 
+  ngOnInit() {    
+		var photoswipeSetupFunction = function () { photoSwipeSetup(); }
+    photoswipeSetupFunction();    
+	}
+}
