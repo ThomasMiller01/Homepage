cf1d523d8e820a0777db24c3b25587b0d7c914c5
Thomas <thomas.miller01.mail@gmail.com>
2019-03-01 14:42:55 +0100

    added logout btn

--- AngularCode/src/app/Components/private/content/home/privateHomeComponent.ts
+++ AngularCode/src/app/Components/private/content/home/privateHomeComponent.ts
@@ -3,4 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   templateUrl: './privateHomeComponent.html'
 })
-export class privateHomeComponent { }
+export class privateHomeComponent { 
+  logout(){
+    localStorage.removeItem("jwt");
+  }  
+}
