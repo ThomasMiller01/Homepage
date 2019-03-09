4fc397fd5a21bc39caf4812c22d922c926d22e50
Thomas Miller <thomas.miller01.mail@gmail.com>
2019-03-09 14:00:46 +0100

    added supervisor status monitor

--- AngularCode/src/app/Components/private/content/home/privateHomeComponent.ts
+++ AngularCode/src/app/Components/private/content/home/privateHomeComponent.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import * as CanvasJS from '../../../../../assets/js/canvasjs.min.js';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+var apiPath = "https://thomasmiller.tk/dotnet/api/";
 
 @Component({
   templateUrl: './privateHomeComponent.html'
 })
 export class privateHomeComponent { 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private http: HttpClient) { }
+
+  supervisorStatus: any;  
+  
 
   logout(){
     this.router.navigate(['/home']);
@@ -14,9 +20,44 @@ export class privateHomeComponent {
   }  
 
   ngOnInit() {
+    this.getSupervisorStatus();
     this.renderColumnChart();
     this.renderPieChart();
     this.renderHighPerformanceChart();
+  }
+
+  startSupervisorService(service){
+    console.log("Start of " + service + " was clicked")
+  }
+
+  stopSupervisorService(service){
+    console.log("Stop of " + service + " was clicked")
+  }
+
+  restartSupervisorService(service){
+    console.log("Restart of " + service + " was clicked")
+  }
+
+  getSupervisorStatus(){      
+    var token = localStorage.getItem("jwt");      
+    var _method = "";
+    this.http.get<any>(apiPath + "supervisor/status", {
+      headers: new HttpHeaders({
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/json"
+      })
+    }).subscribe(response => {	       
+      var replaced = response.replace('"[{', '').replace('}]"', '').replace(new RegExp("'", 'g'), "\"");                        
+      var splited = replaced.split('}, {');      
+      var jsonArray = [];
+      splited.forEach(element => {        
+        var jsonParsed = JSON.parse("{" + element + "}");        
+        jsonArray.push(jsonParsed);
+      });
+      this.supervisorStatus = jsonArray;
+    }, err => {
+      console.log("Error: " + err);
+    });
   }
 
   renderColumnChart(){
