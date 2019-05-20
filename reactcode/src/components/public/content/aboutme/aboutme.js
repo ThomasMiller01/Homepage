de477a3acd43db3ca916e507a3eced471c671a5e
Thomas Miller <thomas.miller01.mail@gmail.com>
2019-05-20 18:29:44 +0200

    feat: about me page

--- reactcode/src/components/public/content/aboutme/aboutme.js
+++ reactcode/src/components/public/content/aboutme/aboutme.js
@@ -7,21 +7,119 @@ const AboutMe = () => {
   return (
     <React.Fragment>
       <Header />
-      <div style={aboutmeDivStyle}>
-        <center>
-          <h1>About Me</h1>
-        </center>
+      <div style={topDivStyle}>
+        <div style={topTextDivStyle}>
+          <h1 style={topH1Style}>
+            <span style={topSpanStyle}>Thomas</span> Miller
+          </h1>
+        </div>
+      </div>
+      <div style={aboutmeDivStyle} className="aboutMeDivStyle">
+        <div style={imageDivStyle} className="imageDivStyle">
+          <img
+            src="http://pixelpraktikum.000webhostapp.com/img/profile/1/profile_pic.png"
+            alt="Error while loading ..."
+            style={imgStyle}
+            className="imgStyle"
+          />
+        </div>
+        <div style={descriptionDivStyle} className="descriptionDivStyle">
+          <h1>Über mich</h1>
+          <p style={pDescriptionStyle}>
+            Mein Name ist Thomas Miller und ich bin {alter()} Jahre alt. Zurzeit
+            gehe ich noch zur Schule und mache nächstes Jahr mein Abitur.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            <br />
+            <br />
+            <span style={spanStyle}>
+              Mich kann man unter{" "}
+              <a href="mailto:info@thomasmiller.tk">info@thomasmiller.tk</a>{" "}
+              erreichen.
+            </span>
+          </p>
+        </div>
       </div>
       <Footer />
     </React.Fragment>
   );
 };
 
+const topH1Style = {
+  color: "#E2E07B",
+  marginBottom: "0",
+  fontFamily: "'Work Sans',sans-serif"
+};
+
+const topSpanStyle = {
+  color: "#76D5D1",
+  width: "0",
+  margin: "0",
+  height: "0"
+};
+
+const topTextDivStyle = {
+  position: "absolute",
+  top: "19%",
+  width: "100%",
+  textAlign: "center",
+  fontFamily: "DejaVuSansMono"
+};
+
+const spanStyle = {
+  fontSize: "22px"
+};
+
+const alter = () => {
+  var birthday = +new Date("2001-12-10");
+  return ~~((Date.now() - birthday) / 31557600000);
+};
+
+const pDescriptionStyle = {
+  fontSize: "24px",
+  lineHeight: "30px"
+};
+
+const imgStyle = {
+  borderStyle: "solid",
+  borderRadius: "100%"
+};
+
+const descriptionDivStyle = {
+  gridColumn: "2",
+  height: "100%",
+  padding: "20px"
+};
+
+const imageDivStyle = {
+  gridColumn: "1",
+  margin: "0",
+  textAlign: "right",
+  padding: "20px"
+};
+
+const topDivStyle = {
+  width: "100%",
+  minHeight: "30vh",
+  backgroundColor: "#272822",
+  padding: "0"
+};
+
 const aboutmeDivStyle = {
   width: "100%",
-  minHeight: "93.5vh",
+  minHeight: "63.5vh",
   backgroundColor: "rgb(230, 230, 230)",
-  padding: "10px"
+  padding: "25px",
+  gridTemplateColumns: "40% 60%"
 };
 
 export default AboutMe;
