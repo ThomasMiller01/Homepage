e78e2892321df4b9f97793cc6def6a2c6e8a6547
Thomas Miller <thomas.miller01.mail@gmail.com>
2019-06-11 20:46:23 +0200

    feat: blog entry style

--- reactcode/src/components/public/content/blog/blog.js
+++ reactcode/src/components/public/content/blog/blog.js
@@ -10,8 +10,15 @@ import "./blog.scss";
 class Blog extends Component {
   state = {
     meta: {},
-    data: []
+    data: [],
+    dateformat: null
   };
+
+  componentWillMount() {
+    const dateformat = require("dateformat");
+    this.setState({ dateformat });
+  }
+
   async componentDidMount() {
     const { match } = this.props;
     let page = match.params.page || 1;
@@ -19,6 +26,30 @@ class Blog extends Component {
     const resp = await butter.post.list({ page: page, page_size: 10 });
     this.setState(resp.data);
   }
+
+  getTagsRendered = tags => {
+    return "";
+    // var _tags = [];
+    // tags.forEach(tag => {
+    //   _tags.push(tag.name);
+    // });
+    // return <span>Tags: {_tags.join(", ")}</span>;
+  };
+
+  getFirstBodyPartRendered = body => {
+    return (
+      <span
+        dangerouslySetInnerHTML={{ __html: body.substring(0, 300) + " ..." }}
+      />
+    );
+  };
+
+  getDateRendered = date => {
+    let _date = new Date(date);
+    let _formatDate = this.state.dateformat(_date, "yyyy-mm-dd hh:MM");
+    return <span>{_formatDate}</span>;
+  };
+
   render() {
     const { next_page, previous_page } = this.state.meta;
 
@@ -26,25 +57,68 @@ class Blog extends Component {
       <React.Fragment>
         <Header />
         <div style={blogStyle}>
-          {this.state.data.map((post, key) => {
-            return (
-              <div key={key}>
-                <Link to={`/blog/posts/${post.slug}`}>{post.title}</Link>
-              </div>
-            );
-          })}
+          <h1 style={{ textAlign: "center" }}>Thomas Blog</h1>
+          <div style={borderBottomStyle} />
+          <div style={blogPostsStyle}>
+            {this.state.data.map((post, key) => {
+              return (
+                <React.Fragment key={key}>
+                  <div className="card" style={cardStyle}>
+                    <div className="card-body">
+                      <h3 className="card-title" style={cardTitleStyle}>
+                        {post.title}
+                      </h3>
+                      <h6 className="card-subtitle mb-2 text-muted" style={cardMetaStyle}>
+                        {post.author.first_name} {post.author.last_name},{" "}
+                        {this.getDateRendered(post.published)}
+                      </h6>
+                      <h6 className="card-subtitle mb-2 text-muted">
+                        {this.getTagsRendered(post.tags)}
+                      </h6>
+                      <p className="card-text">
+                        {this.getFirstBodyPartRendered(post.body)}
+                      </p>
+                      <Link
+                        to={`/blog/posts/${post.slug}`}
+                        className="btn btn-outline-primary"
+                      >
+                        Read more
+                      </Link>
+                    </div>
+                  </div>
+                  <div style={borderBottomStyle2} />
+                </React.Fragment>
+              );
+            })}
+          </div>
+          <div style={rightCardStyle}>
+            <h1>RightCard</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet.
+            </p>
+          </div>
 
           <br />
 
           <div>
             {previous_page && (
-              <Link to={`/blog/${previous_page}`}>
+              <Link
+                to={`/blog/${previous_page}`}
+                className="btn btn-outline-primary"
+              >
                 <span>Prev</span>
               </Link>
             )}
 
             {next_page && (
-              <Link to={`/blog/${next_page}`}>
+              <Link
+                to={`/blog/${next_page}`}
+                className="btn btn-outline-primary"
+              >
                 <span>Next</span>
               </Link>
             )}
@@ -56,11 +130,62 @@ class Blog extends Component {
   }
 }
 
+const cardMetaStyle = {
+  width: "30%",
+  margin: "0",
+  display: "inline-block"
+}
+
+const cardTitleStyle = {
+  width: "70%",
+  display: "inline-block",
+  margin: "0"
+};
+
+const rightCardStyle = {
+  width: "15%",
+  margin: "10px 5% 10px 5%",
+  display: "inline-block",
+  verticalAlign: "top",
+  minHeight: "60vh",
+  padding: "10px"
+};
+
+const blogPostsStyle = {
+  width: "65%",
+  margin: "10px 0 10px 10%",
+  display: "inline-block",
+  padding: "10px"
+};
+
+const cardStyle = {
+  width: "100%",
+  marginBottom: "10px",
+  marginTop: "10px",
+  // backgroundColor: "#f2f2f2",
+  backgroundColor: "#E6E6E6",
+  border: "none"
+};
+
 const blogStyle = {
   width: "100%",
   minHeight: "87vh",
   backgroundColor: "#E6E6E6",
   padding: "20px"
+};
+
+const borderBottomStyle = {
+  width: "95%",
+  margin: "10px auto 10px auto",
+  height: "2px",
+  borderBottom: "solid 2px rgb(161, 161, 161)"
+};
+
+const borderBottomStyle2 = {
+  width: "100%",
+  margin: "10px auto 10px auto",
+  height: "1px",
+  borderBottom: "solid 1px rgb(161, 161, 161)"
 };
 
 export default Blog;
