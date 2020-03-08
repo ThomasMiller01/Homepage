6bfc75a9e98f1e3f8cd23c6f25a2c84b440c8a5b
Thomas Miller <thomas.miller01.mail@gmail.com>
2020-03-08 21:25:04 +0100

    feat: new editor: tiny cloud editor

---
+++ reactcode/src/components/editor/tiny_editor.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+
+class TinyEditor extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleContentChange = props.handleContentChange.bind(this);
+    this.handleEditorChange = this.handleEditorChange.bind(this);
+
+    let init_default = {
+      height: 400,
+      plugins: [
+        "code advlist anchor autolink codesample fullscreen help image imagetools",
+        "lists link media noneditable preview",
+        "searchreplace table template visualblocks wordcount"
+      ],
+      toolbar:
+        "insertfile a11ycheck undo redo | bold italic | forecolor backcolor | codesample | alignleft aligncenter alignright alignjustify | bullist numlist | link image tinydrive"
+    };
+
+    this.init = props.init ? props.init : init_default;
+    this.value = props.init_value ? props.init_value : "";
+  }
+
+  handleEditorChange = (content, editor) => {
+    this.value = content;
+    this.handleContentChange(content);
+  };
+
+  updateContent = content => {
+    this.value = content;
+  };
+
+  render() {
+    return (
+      <Editor
+        value={this.value}
+        apiKey="98olmh8w58ecwa02rwezp6u8nklm2c6wo87i9yt42dckjl3u"
+        init={this.init}
+        onEditorChange={this.handleEditorChange}
+      />
+    );
+  }
+}
+
+export default TinyEditor;
