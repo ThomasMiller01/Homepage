29fa2562f364d587a616cb16d556f4799c9789f0
Thomas Miller <thomas.miller01.mail@gmail.com>
2020-06-05 15:42:36 +0200

    refractor: healtchcheck api url

--- reactcode/src/components/api_urls.js
+++ reactcode/src/components/api_urls.js
@@ -1,6 +1,5 @@
 const auth_url = "https://api.thomasmiller.info/auth";
 const homepage_url = "https://api.thomasmiller.info/homepage";
-//const healthcheck_url = "https://api.thomasmiller.info/healthcheck";
-const healthcheck_url = "http://localhost:5003/graphql";
+const healthcheck_url = "https://api.thomasmiller.info/healthcheck";
 
 export { auth_url, homepage_url, healthcheck_url };
