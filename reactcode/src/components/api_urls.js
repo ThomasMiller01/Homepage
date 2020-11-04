60e9f9285a634331ca503573b35d34fe88064ed6
Thomas Miller <thomas.miller01.mail@gmail.com>
2020-11-04 17:39:15 +0100

    Update api_urls.js

--- reactcode/src/components/api_urls.js
+++ reactcode/src/components/api_urls.js
@@ -3,4 +3,6 @@ const homepage_url = "https://api.thomasmiller.info/homepage";
 const healthcheck_url = "https://api.thomasmiller.info/healthcheck";
 const eventregistry_url = "https://api.thomasmiller.info/eventregistry";
 
+// const homepage_url = "http://localhost:5006/graphql";
+
 export { auth_url, homepage_url, healthcheck_url, eventregistry_url };
