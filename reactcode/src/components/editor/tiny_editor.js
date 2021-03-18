e0d5a8863f5cb82bfd84a4422be6d37988cf7162
Thomas Miller <thomas.miller01.mail@gmail.com>
2021-03-18 23:01:38 +0100

    Update tiny_editor.js

--- reactcode/src/components/editor/tiny_editor.js
+++ reactcode/src/components/editor/tiny_editor.js
@@ -9,14 +9,68 @@ class TinyEditor extends Component {
     this.handleEditorChange = this.handleEditorChange.bind(this);
 
     let init_default = {
-      height: 400,
+      min_height: 400,
       plugins: [
-        "code advlist anchor autolink codesample fullscreen help image imagetools",
-        "lists link media noneditable preview",
-        "searchreplace table template visualblocks wordcount"
+        "lists advlist autolink anchor autoresize codesample help hr image imagetools link media preview print quickbars searchreplace table textpattern toc visualblocks wordcount",
       ],
       toolbar:
-        "undo redo | bold italic | forecolor backcolor | codesample | alignleft aligncenter alignright alignjustify | bullist numlist | link image"
+        "undo redo | bold italic underline | formatselect  | alignleft aligncenter alignright alignjustify | bullist numlist | table link",
+      quickbars_selection_toolbar:
+        "bold italic underline | alignleft aligncenter alignright alignjustify | blockquote",
+      fontsize_formats: "8pt 9pt 10pt 11pt 12pt 14pt 16pt 18pt 24pt 36pt 48pt",
+      codesample_languages: [
+        { text: "Python", value: "python" },
+        { text: "C#", value: "csharp" },
+        { text: "HTML/XML", value: "markup" },
+        { text: "JavaScript", value: "javascript" },
+        { text: "Java", value: "java" },
+        { text: "CSS", value: "css" },
+        { text: "C", value: "c" },
+        { text: "C++", value: "cpp" },
+      ],
+      link_list: [
+        {
+          title: "info@thomasmiller.info",
+          value: "mailto:info@thomasmiller.info",
+        },
+        { title: "GitHub", value: "https://github.com/ThomasMiller0" },
+        {
+          title: "Xing",
+          value: "https://www.xing.com/profile/Thomas_Miller108",
+        },
+        {
+          title: "LinkedIn",
+          value: "https://www.linkedin.com/in/thomas-miller-2a13991b1",
+        },
+        { title: "My Blog", value: "https://thomasmiller.info/blog" },
+      ],
+      textpattern_patterns: [
+        { start: "*", end: "*", format: "italic" },
+        { start: "**", end: "**", format: "bold" },
+        { start: "__", end: "__", format: "underline" },
+        { start: "~", end: "~", format: "blockquote" },
+        { start: "#", format: "h1" },
+        { start: "##", format: "h2" },
+        { start: "###", format: "h3" },
+        { start: "- ", cmd: "InsertUnorderedList" },
+        { start: "* ", cmd: "InsertUnorderedList" },
+        {
+          start: "1. ",
+          cmd: "InsertOrderedList",
+          value: { "list-style-type": "decimal" },
+        },
+        { start: "---", replacement: "<hr/>" },
+      ],
+      toc_depth: 4,
+      toolbar_mode: "wrap",
+      toolbar_sticky: true,
+      branding: false,
+      draggable_modal: true,
+      relative_urls: false,
+      link_assume_external_targets: "https",
+      link_title: false,
+      quickbars_image_toolbar: false,
+      quickbars_insert_toolbar: false,
     };
 
     this.init = props.init ? props.init : init_default;
@@ -28,7 +82,7 @@ class TinyEditor extends Component {
     this.handleContentChange(content);
   };
 
-  updateContent = content => {
+  updateContent = (content) => {
     this.value = content;
   };
 
