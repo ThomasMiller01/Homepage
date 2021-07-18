74a4c5d63878563b18a0d5e2cd4e023fb0335440
Thomas Miller <thomas.miller01.mail@gmail.com>
2021-07-18 21:37:55 +0200

    basic sitemap lib + script

--- reactcode/src/App.js
+++ reactcode/src/App.js
@@ -1,60 +1,6 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import Home from "./components/public/content/home/home";
-import Projects from "./components/public/content/projects/projects";
-import AboutMe from "./components/public/content/aboutme/aboutme";
-import Blog from "./components/public/content/blog/blog";
-import BlogPost from "./components/public/content/blog/blog_post";
-import blog_category from "./components/public/content/blog/blog_category";
-import blog_categories from "./components/public/content/blog/blog_categories";
-import Impressum from "./components/public/content/impressum/impressum";
-import Private from "./components/private/private";
-import Login from "./components/public/login";
-import Error from "./components/error";
-import ProtectedRoute from "./components/ProtectedRoute";
-import ScrollHandler from "./components/scrollHandler";
-
-import ColorThemeHook from "./components/color-theme/color_theme_hook";
-
-const Router = ({ history }) => {
-  return (
-    <BrowserRouter history={history}>
-      <ColorThemeHook>
-        <ScrollHandler>
-          <Switch>
-            <Redirect exact from="/" to="/home" />
-            <Route path="/home" component={Home} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/about" component={AboutMe} />
-            <Route path="/blog" exact component={Blog} />
-            <Route path="/blog/categories" exact component={blog_categories} />
-            <Route path="/blog/:page" exact component={Blog} />
-            <Route path="/blog/posts/:post" exact component={BlogPost} />
-            <Route
-              path="/blog/category/:category"
-              exact
-              component={blog_category}
-            />
-            <Route path="/impressum" component={Impressum} />
-            <Route path="/login" component={Login} />
-            <ProtectedRoute path="/private" component={Private} />
-            <Route
-              render={() => (
-                <Error
-                  file="App.js"
-                  error_title="Bad Request"
-                  error_number="404"
-                  error_description="The server cannot process the request due to something that is perceived to be a client error."
-                />
-              )}
-            />
-          </Switch>
-        </ScrollHandler>
-      </ColorThemeHook>
-    </BrowserRouter>
-  );
-};
+import Router from "./components/router";
 
 const App = () => {
   return (
---
+++ reactcode/src/components/router.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import Home from "./public/content/home/home";
+import Projects from "./public/content/projects/projects";
+import AboutMe from "./public/content/aboutme/aboutme";
+import Blog from "./public/content/blog/blog";
+import BlogPost from "./public/content/blog/blog_post";
+import blog_category from "./public/content/blog/blog_category";
+import blog_categories from "./public/content/blog/blog_categories";
+import Impressum from "./public/content/impressum/impressum";
+import Private from "./private/private";
+import Login from "./public/login";
+import Error from "./error";
+import ProtectedRoute from "./ProtectedRoute";
+import ScrollHandler from "./scrollHandler";
+
+import ColorThemeHook from "./color-theme/color_theme_hook";
+
+const Router = ({ history }) => {
+  return (
+    <BrowserRouter history={history}>
+      <ColorThemeHook>
+        <ScrollHandler>
+          <Switch>
+            <Redirect exact from="/" to="/home" />
+            <Route path="/home" component={Home} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/about" component={AboutMe} />
+            <Route path="/blog" exact component={Blog} />
+            <Route path="/blog/categories" exact component={blog_categories} />
+            <Route path="/blog/:page" exact component={Blog} />
+            <Route path="/blog/posts/:post" exact component={BlogPost} />
+            <Route
+              path="/blog/category/:category"
+              exact
+              component={blog_category}
+            />
+            <Route path="/impressum" component={Impressum} />
+            <Route path="/login" component={Login} />
+            <ProtectedRoute path="/private" component={Private} />
+            <Route
+              render={() => (
+                <Error
+                  file="App.js"
+                  error_title="Bad Request"
+                  error_number="404"
+                  error_description="The server cannot process the request due to something that is perceived to be a client error."
+                />
+              )}
+            />
+          </Switch>
+        </ScrollHandler>
+      </ColorThemeHook>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
